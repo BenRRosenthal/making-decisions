1656f2bdd5c72df8aef39efca78607d171ab1474
Benjamin rosenthal <benrrosenthal@gmail.com>
2022-07-05 19:26:35 -0600

    Problem 5-7 completed

--- lab.js
+++ lab.js
@@ -85,7 +85,9 @@ let rain = true;
 */
 
 //CODE HERE
-
+for (let i = 0; i < 11; i++){
+  console.log(i)
+}
 ////////// PROBLEM 6 //////////
 
 /*
@@ -93,7 +95,9 @@ let rain = true;
 */
 
 //CODE HERE
-
+for (let i = 1; i < 11; i++){
+  console.log(i)
+}
 ////////// PROBLEM 7 //////////
 
 /*
@@ -101,7 +105,9 @@ let rain = true;
 */
 
 //CODE HERE
-
+for (let i = 10; i > 0; i--){
+  console.log(i)
+}
 
 ////////// PROBLEM 8 //////////
 
